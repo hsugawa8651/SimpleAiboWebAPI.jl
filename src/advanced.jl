afbd82df19fcf471d60011ff0404fb82974400eb
Hiroharu Sugawara <hsugawa@gmail.com>
2021-08-08 17:59:29 +0900

    [BREAKING] askMoveHead: azimuth, elevation, velocity, and enqueue changed to optional keyword argument

--- src/advanced.jl
+++ src/advanced.jl
@@ -190,11 +190,11 @@ end
 
 
 """
-    askMoveHead(
+    askMoveHead(;
       azimuth=0,
       elevation=0,
       velocity=0,
-      enqueue=false;
+      enqueue=false,
       target_deviceID=nothing,
       target_nickname=nothing,
       timeoutLimit=10)
@@ -208,11 +208,11 @@ asks `move_head`.
 This method is equivalent to `askAction("move_head", Dict(Duration=>duration, Enqueue=>enqueue))`
 
 """
-function askMoveHead(
+function askMoveHead(;
    azimuth=0,
    elevation=0,
    velocity=0,
-   enqueue=false;
+   enqueue=false,
    target_deviceID=nothing,
    target_nickname=nothing,
 	timeoutLimit=10)
